afdc8031d563cc773e0d28d24f8e6d775cf08fa2
tfmilton <43272039+tfmilton@users.noreply.github.com>
2024-11-07 17:20:08 -0600

    Add files via upload

---
+++ headers for spatial sim/macro_dynamic.h
@@ -0,0 +1,3 @@
+int *speciate(int**, int*, int*, int*, int*, int*);
+
+void replace(int**, int, int, int*, int*, int*, double*, int, int*);
---
+++ headers for spatial sim/time_keeper.h
@@ -0,0 +1,5 @@
+void start_time(void);
+
+void prn_time(void);
+
+void prn_total_time(void);
